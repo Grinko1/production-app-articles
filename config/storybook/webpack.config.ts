3789008ec7bb98bfc00604efefd4e74977302bed
Grinko1 <86093873+Grinko1@users.noreply.github.com>
2023-11-07 10:30:09 +0900

    fix storybook webpack config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/prefer-includes */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 import { type RuleSetRule } from 'webpack';
 import type webpack from 'webpack';
 import path from 'path';
@@ -14,13 +17,20 @@ export default ({ config }: { config: webpack.Configuration }) => {
   config.resolve?.modules?.push(paths.src);
   config.resolve?.extensions?.push('.ts', '.tsx');
 
-  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
-    if ((rule.test as string).includes('svg')) {
+  config!.module!.rules = config?.module!.rules!.map((rule: RuleSetRule) => {
+    if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
     }
 
     return rule;
   });
+  // config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+  //   if ((rule.test as string).includes('svg')) {
+  //     return { ...rule, exclude: /\.svg$/i };
+  //   }
+
+  //   return rule;
+  // });
 
   config.module.rules.push({
     test: /\.svg$/,
